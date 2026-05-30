bd5829a0c068b215e554b860f2b16a4c4109668a
Ercument <ercument_eskar@hotmail.com>
2026-05-31 01:07:13 +0700

    release: bump versionCode to 98

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ versionProps.load(FileInputStream(versionPropsFile))
 
 val currentVersionCode = versionProps["versionCode"].toString().toInt()
 
-val currentVersionName = "1.0.37"
+val currentVersionName = "1.0.38"
 
 android {
     namespace = "com.verifyblind.mobile"
