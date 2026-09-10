06a6146bf6e09e7484564eef062b964460ebc56c
Ercument <4041591+VerifyBlind@users.noreply.github.com>
2026-09-11 04:47:34 +0700

    release: v1.0.194 (versionCode 260)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ versionProps.load(FileInputStream(versionPropsFile))
 
 val currentVersionCode = versionProps["versionCode"].toString().toInt()
 
-val currentVersionName = "1.0.193"
+val currentVersionName = "1.0.194"
 
 android {
     namespace = "com.verifyblind.mobile"
